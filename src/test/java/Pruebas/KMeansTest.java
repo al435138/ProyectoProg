3a9f1325f271eb9c07268e21b7885b1f5868a5f7
al435138 <al435138@uji.es>
2025-03-18 12:03:50 +0100

    Cambio getLabel

--- src/test/java/Pruebas/KMeansTest.java
+++ src/test/java/Pruebas/KMeansTest.java
@@ -68,7 +68,7 @@ class KMeansTest {
         kMeans = new KMeans(200, numIterations, seed);
         Exception e = assertThrows(InvalidClusterNumberException.class, () -> kMeans.train(iris));
         // TODO: reemplazar getNumRows() con método equivalente, si hace falta
-        System.out.println("Clusters: "+((InvalidClusterNumberException)e).getNumberOfCusters());
-        assertTrue(((InvalidClusterNumberException)e).getNumberOfCusters() > iris.getNumRows());
+        System.out.println("Clusters: "+((InvalidClusterNumberException)e).getNumberOfClusters());
+        assertTrue(((InvalidClusterNumberException)e).getNumberOfClusters() > iris.getRowCount());
     }
 }
