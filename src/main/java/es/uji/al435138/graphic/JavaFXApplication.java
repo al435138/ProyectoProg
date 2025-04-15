d089127ca15e893936caaa1367a69ceb05b371b5
Luis Eres Vidal <luiservi05@gmail.com>
2025-04-15 11:28:00 +0200

    Añadida clase JavaFXApplication

---
+++ src/main/java/es/uji/al435138/graphic/JavaFXApplication.java
@@ -0,0 +1,11 @@
+package es.uji.al435138.graphic;
+
+public class JavaFXApplication extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start
+
+}
