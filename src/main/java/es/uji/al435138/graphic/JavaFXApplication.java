305f882fc5105d25b6eeb0385476489e6ad19cf1
Luis Eres Vidal <luiservi05@gmail.com>
2025-04-15 11:30:21 +0200

    Prueba de ejemplo javafx

--- src/main/java/es/uji/al435138/graphic/JavaFXApplication.java
+++ src/main/java/es/uji/al435138/graphic/JavaFXApplication.java
@@ -1,11 +1,25 @@
 package es.uji.al435138.graphic;
 
-public class JavaFXApplication extends Application{
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
-    public static void main(String[] args) {
-        launch(args);
+public class JavaFXApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        VBox layout = new VBox(); // Contenedor principal
+        Label label = new Label("Hola mundo!"); // Texto no editable
+        Button button = new Button("Un botón :D"); // Un botón, no hace nada
+        layout.getChildren().addAll(label, button); // Añade elementos al contenedor
+        layout.setAlignment(Pos.CENTER); // Centra los elementos
+// Añade el contenedor a la escena, y la escena al escenario
+        primaryStage.setScene(new Scene(layout, 200, 100));
+        primaryStage.setTitle("JavaFXApp"); // Título de ventana
+        primaryStage.show(); // Muestra la ventana
     }
-
-    public void start
-
 }
